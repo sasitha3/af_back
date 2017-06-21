5004bddaed0a4b3f74a495fde84f69f9c4d9de58
sasitha3 <sasitha.premadasa@my.sliit.lk>
2017-06-21 17:45:32 +0530

    order list details done

--- controller/order_controller.js
+++ controller/order_controller.js
@@ -1,18 +1,22 @@
 const Order = require('../models/orders/order');
 
 function insert(req, res){
-    const user = new Order({
+    const order = new Order({
         name: req.body.name
 
     });
 
-    user.save()
+    order.save()
         .then(savedUser => res.json(savedUser))
         .catch(e => next(e));
 }
 
 function list(req, res){
-    res.send('get Val');
+    Order.find(function (err, Order) {
+        if (err) return console.error(err);
+       // console.log(Order);
+        res.send(Order);
+    });
 }
 
 module.exports = {insert, list}
