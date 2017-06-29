1dc36a3300f7b3dc546665c3b94666d229ae4236
sasitha3 <sasitha.premadasa@my.sliit.lk>
2017-06-30 02:05:02 +0530

    add CORS headers

--- index.js
+++ index.js
@@ -13,18 +13,27 @@ mongoose.Promise = global.Promise;
 
 // connect to mongo db
 const mongoUri = config.mongo.host;
-mongoose.connect(mongoUri, { server: { socketOptions: { keepAlive: 1 } } });
+mongoose.connect(mongoUri, { server: { socketOptions: { keepAlive: 1 } } }, function () {
+    console.log('connected to the database');
+});
 mongoose.connection.on('error', () => {
     throw new Error(`unable to connect to database: ${mongoUri}`);
 });
 
 app.use(bodyParser.json());
 
+//CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 //intialize route
 app.use('/api',route);
 
 //listen for request
 app.listen( process.env.port || config.port, function () {
-    console.log('Now listning for request');
+    console.log('Now listening for request on port ' + config.port);
 });
-
