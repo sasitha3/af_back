278381f4f37f2758886fa8d8a92fa6b273ad3a2b
lakithC <lakith.siriwardana@my.sliit.lk>
2017-07-01 00:03:49 +0530

    lakith first message

---
+++ controller/medicine_controller.js
@@ -0,0 +1,20 @@
+const medic = require('../models/prescription/medicine');
+
+function insert(req, res){
+    const drug = new medic({
+        drugname: req.body.drugname
+
+    });
+
+    drug.save()
+        .then(savedUser => res.json(savedUser))
+        .catch(e => next(e));
+}
+function list(req, res){
+    medic.find(function (err, medic) {
+        if (err) return console.error(err);
+       // console.log(Order);
+        res.send(medic);
+    });
+}
+module.exports = {insert: list }
--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const order = require('./order_route');
+const prescription = require('./ShowPrescript');
+
 
 //your routers
 router.use('/orders', order);
+router.use('/prescription', prescription);
 
 module.exports = router;
 
