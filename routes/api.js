19e50dffd2e7b9d194fae906b7298f58e66c67df
Lasini Liyanage <lasini.1021@gmail.com>
2017-07-01 16:46:40 +0530

    merged apis

--- routes/api.js
+++ routes/api.js
@@ -6,10 +6,13 @@ const prescription = require('./ShowPrescript');
 const  DrugData=require('./DrugDataManage');
 const stock = require('./stock_route');
 
+
+
 //your routers
 router.use('/orders', order);
 router.use('/prescription', prescription);
 router.use('/DrugData',DrugData);
 router.use('/stock',stock);
+
 module.exports = router;
 
