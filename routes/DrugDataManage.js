bb9274360d6f418deea362cf76b99a8f85b516ea
sacheen <sacheenwarnaka863@gmail.com>
2017-07-01 13:25:25 +0530

    sacheen 1st commit

---
+++ routes/DrugDataManage.js
@@ -0,0 +1,3 @@
+/**
+ * Created by sacheen on 7/1/2017.
+ */
--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,12 @@ const router = express.Router();
 
 const order = require('./order_route');
 const prescription = require('./ShowPrescript');
+const  DrugData=require('./DrugDataManage');
 
 
 //your routers
 router.use('/orders', order);
 router.use('/prescription', prescription);
-
+router.use('/DrugData',DrugData);
 module.exports = router;
 
