0ce669907c90f3976cb6aef9298e7398c00612cc
lakithC <lakith.siriwardana@my.sliit.lk>
2017-07-02 13:18:57 +0530

    my 2

--- routes/api.js
+++ routes/api.js
@@ -2,18 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const order = require('./order_route');
-const doctor = require('./doctor_route');
-const medicine = require('./medicine_route');
-const display = require('./showprescript_route');
-const dispense = require('./dispenseprescript_route');
+const prescription = require('./prescription_route');
 
 
 //your routers
 router.use('/orders', order);
-router.use('/doctor', doctor);
-router.use('/medicine', medicine);
-router.use('/display', display);
-router.use('/dispense', dispense);
+router.use('/prescription', prescription);
 
 module.exports = router;
 
