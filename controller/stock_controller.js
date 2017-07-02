0c814029e7d21d24fdb3fd03df18d482fe00d73d
Lasini Liyanage <lasini.1021@gmail.com>
2017-07-02 13:20:34 +0530

    controller modified

--- controller/stock_controller.js
+++ controller/stock_controller.js
@@ -39,12 +39,14 @@ function viewRequests(req, res){
     });
 }
 
-function drugTableLoad(req, res){
-    DrugBatch.find(function (err, DrugBatch) {
-        if (err) return console.error(err);
-        console.log(DrugBatch);
-        res.send(DrugBatch);
 
+
+function drugTableLoad(req, res){
+    DrugBatch.find({ expiryDate: { $lte: new Date() } }).then(function (values) {
+        console.log(values);
+        res.send(values);
+    }).catch(function (err) {
+        console.log(err);
     });
 }
 
@@ -60,10 +62,41 @@ function deleteRequest(req,res) {
     });
 }
 
+function showconfirmedRequests(req, res){
+    Request.find({ status:"Computed" }).then(function (values) {
+        console.log(values);
+        res.send(values);
+    }).catch(function (err) {
+        console.log(err);
+    });
+}
+
+function updateRequest(req, res){
+    var id = req.params.id;
+    console.log(req.body.requestId);
+    Request.findAndModify({query: { drugName: req.body.drugName },
+            update: {$set: {status: "Confirmed", quantity: req.body.quantity}}, new: true},
+        function(err,doc){
+            res.json(doc);
+        }
+    );
+}
 
 
+//
+// var result = jsObjects.filter(function( obj ) {
+//     return obj.b == 6;
+// });
+//
+// var dbinsSelect = {};
+//
+// function filterlist(req, res){
+//     DrugBatch.find(function (err, DrugBatch) {
+//         if (err) return console.error(err);
+//         console.log(DrugBatch);
+//         res.send(DrugBatch);
+//
+//     });
+// }
 
-
-
-
-module.exports = {insert, list,addBatch,viewRequests,drugTableLoad,deleteRequest};
+module.exports = {insert, list,addBatch,viewRequests,deleteRequest,drugTableLoad,showconfirmedRequests,updateRequest};
